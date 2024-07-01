7f58f1ff6877da493eb494c807df55e7387276ab
K T Likhith <ktlikhith@Ks-MacBook-Air.local>
2024-07-01 16:02:11 +0530

    Added the learning report

---
+++ elearning/lib/ui/Reports/Learning_Report/learning_detail.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'package:elearning/services/learninpath_service.dart';
+
+class LearningPathDetailScreen extends StatelessWidget {
+  final LearningPathDetail learningPath;
+
+  LearningPathDetailScreen({required this.learningPath});
+
+  @override
+  Widget build(BuildContext context) {
+    List<LearningPathProgress> courses = learningPath.learningpathProgress;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(learningPath.name),
+        backgroundColor: Theme.of(context).primaryColor,
+        centerTitle: false,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      backgroundColor: Theme.of(context).backgroundColor,
+      body: courses.isEmpty
+          ? Center(child: Text('No courses available'))
+          : ListView.builder(
+              itemCount: courses.length,
+              itemBuilder: (context, index) {
+                var course = courses[index];
+                return GestureDetector(
+                  onTap: () {
+                    // Navigate to course details screen if needed
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.3),
+                            spreadRadius: 1,
+                            blurRadius: 4,
+                            offset: Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              course.name,
+                              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(height: 8),
+                            Row(
+                              children: [
+                                Stack(
+                                  children: [
+                                    Container(
+                                      height: 20,
+                                      width: MediaQuery.of(context).size.width * 0.8,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10),
+                                        gradient: LinearGradient(
+                                          colors: [
+                                            Color(0xFF3ACBE8).withOpacity(0.3),
+                                            Color(0xFF0D85D8).withOpacity(0.3),
+                                            Color(0xFF0041C7).withOpacity(0.3),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                    Container(
+                                      height: 20,
+                                      width: MediaQuery.of(context).size.width * 0.8 * (course.progress / 100),
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10),
+                                        gradient: LinearGradient(
+                                          colors: [
+                                            Color(0xFF3ACBE8),
+                                            Color(0xFF0D85D8),
+                                            Color(0xFF0041C7),
+                                          ],
+                                          stops: [0.0, 0.5, 1.0],
+                                        ),
+                                      ),
+                                    ),
+                                    if (course.progress > 0)
+                                      Positioned(
+                                        left: MediaQuery.of(context).size.width * 0.8 * (course.progress / 100) - 20,
+                                        child: Container(
+                                          width: 20,
+                                          height: 20,
+                                          decoration: BoxDecoration(
+                                            shape: BoxShape.circle,
+                                            gradient: LinearGradient(
+                                              colors: [
+                                                Color(0xFF3ACBE8),
+                                                Color(0xFF0D85D8),
+                                                Color(0xFF0041C7),
+                                              ],
+                                              stops: [0.0, 0.5, 1.0],
+                                            ),
+                                            boxShadow: [
+                                              BoxShadow(
+                                                color: Colors.black.withOpacity(0.1),
+                                                blurRadius: 5,
+                                                offset: Offset(0, 2),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                  ],
+                                ),
+                                SizedBox(width: 8),
+                                Text(
+                                  '${course.progress.toInt()}%',
+                                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 16),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+    );
+  }
+
+  Color _getStatusColor(int progress) {
+    if (progress == 0) {
+      return Colors.red;
+    } else if (progress == 100) {
+      return Colors.green;
+    } else {
+      return Colors.orange;
+    }
+  }
+}
---
+++ elearning/lib/ui/Reports/Learning_Report/learning_path.dart
@@ -0,0 +1,174 @@
+import 'package:elearning/ui/Reports/Learning_Report/learning_detail.dart';
+import 'package:flutter/material.dart';
+import 'package:elearning/services/learninpath_service.dart';
+
+class LearningPathScreen extends StatefulWidget {
+  final String token;
+  const LearningPathScreen({Key? key, required this.token}) : super(key: key);
+
+  @override
+  _LearningPathScreenState createState() => _LearningPathScreenState();
+}
+
+class _LearningPathScreenState extends State<LearningPathScreen> {
+  late Future<Map<String, dynamic>> _learningPathData;
+
+  @override
+  void initState() {
+    super.initState();
+    _learningPathData = LearningPathApiService.fetchLearningPathData(widget.token);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Learning Paths'),
+        backgroundColor: Theme.of(context).primaryColor,
+        centerTitle: false,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      backgroundColor: Theme.of(context).backgroundColor,
+      body: FutureBuilder<Map<String, dynamic>>(
+        future: _learningPathData,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (snapshot.hasData && (snapshot.data!['learningpathdetail'] as List).isEmpty) {
+            return Center(child: Text('No learning paths available'));
+          } else {
+            List learningPaths = snapshot.data!['learningpathdetail'];
+            List progressList = snapshot.data!['learningpath_progress'];
+            return Container(
+ 
+  child: ListView.builder(
+    itemCount: learningPaths.length,
+    itemBuilder: (context, index) {
+      var learningPath = LearningPathDetail.fromJson(learningPaths[index], progressList);
+      return GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => LearningPathDetailScreen(learningPath: learningPath),
+            ),
+          );
+        },
+        child: Container(
+          margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+          padding: EdgeInsets.all(12),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            border: Border.all(
+                // color: Theme.of(context).secondaryHeaderColor.withOpacity(0.4),
+                color:Theme.of(context).cardColor,
+              ),
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.3),
+                spreadRadius: 1,
+                blurRadius: 4,
+                offset: Offset(0, 3),
+              ),
+            ],
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Text(
+                  learningPath.name,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              SizedBox(height: 12),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    children: [
+                      SizedBox(
+                        width: 100,
+                        height: 100,
+                        child: Stack(
+                          fit: StackFit.expand,
+                          children: [
+                            CircularProgressIndicator(
+                              value: learningPath.progress / 100,
+                              strokeWidth: 10,
+                              backgroundColor: Colors.grey[300],
+                              valueColor: AlwaysStoppedAnimation<Color>(
+                                _getStatusColor(learningPath.progress),
+                              ),
+                            ),
+                            Center(
+                              child: Text(
+                                '${learningPath.progress}%',
+                                style: TextStyle(
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        _getStatusText(learningPath.progress),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: _getStatusColor(learningPath.progress),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+    },
+  ),
+);
+
+          }
+        },
+      ),
+    );
+  }
+
+
+String _getStatusText(int progress) {
+    if (progress == 0) {
+      return 'Not Started';
+    } else if (progress == 100) {
+      return 'Completed';
+    } else {
+      return 'In Progress';
+    }
+  }
+
+  Color _getStatusColor(int progress) {
+    if (progress == 0) {
+      return Colors.red;
+    } else if (progress == 100) {
+      return Colors.green;
+    } else {
+      return Colors.orange;
+    }
+  }
+}
